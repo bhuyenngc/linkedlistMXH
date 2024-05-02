217b696497b729ca049d4f8cd647147e1869bba1
bhuyenngc <166475638+bhuyenngc@users.noreply.github.com>
2024-05-02 20:57:04 +0700

    Add files via upload

---
+++ quanLyBaiDangMXH.sql
@@ -0,0 +1,10 @@
+﻿create database quanLyBaiDangMXH
+use quanLyBaiDangMXH
+
+CREATE TABLE baiDang (
+    PostID NVARCHAR(20) PRIMARY KEY ,
+    noiDungBaiDang NVARCHAR(MAX),
+    tacGia NVARCHAR(100),
+    ngayDang NVARCHAR(100),
+);
+ALTER TABLE baiDang ADD N'Hình ảnh' Image
