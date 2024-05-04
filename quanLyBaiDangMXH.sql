e09e79cb6eb8e79d53b6152c0ab5fbc46e2b16e2
bhuyenngc <166475638+bhuyenngc@users.noreply.github.com>
2024-05-04 15:11:42 +0700

    Delete quanLyBaiDangMXH.sql

--- quanLyBaiDangMXH.sql
+++
@@ -1,9 +0,0 @@
-﻿create database quanLyBaiDangMXH
-use quanLyBaiDangMXH
-
-CREATE TABLE baiDang (
-    PostID NVARCHAR(20) PRIMARY KEY ,
-    noiDungBaiDang NVARCHAR(MAX),
-    tacGia NVARCHAR(100),
-    ngayDang NVARCHAR(100),
-);
